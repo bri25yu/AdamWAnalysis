433f9cae6ab8314d778667c6c880f131a8635ddb
bri25yu <bri25yu@gmail.com>
2022-11-09 17:37:04 -0800

    adding results; removing unnecessary vis logic; adding to readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # AdamWAnalysis
+
+### Exact algorithm
+![](results/ExactAdamWExperiment/benchmark.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+### Learning which centers correspond to which class
+![](results/CenterLabelsAdamWExperiment/benchmark.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+### And learning that values closer to 1 are better
+![](results/LearnOffsetAdamWExperiment/benchmark.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+### And learning that larger scales leads to better softmax outputs
+![](results/OffsetScaleAdamWExperiment/benchmark.gif)
+
+<div style="page-break-after: always;"></div>
--- scripts/vis_env.py
+++ scripts/vis_env.py
@@ -1,9 +1,24 @@
-from numpy.random import seed
+import os
 
+import matplotlib.pyplot as plt
+from seaborn import scatterplot
+
+from awa import VIS_OUPTUT_DIR
 from awa.infra import Env
-from awa.vis.env import plot_env
 
 
-seed(42)
-env = Env(N=100000, C=2, D=2)
-plot_env(env)
+def plot_env(env: Env) -> None:
+    rows, cols = 1, 1
+    fig, ax = plt.subplots(rows, cols, figsize=(10 * cols, 8 * rows))
+
+    xs = env.points[:, 0]
+    ys = env.points[:, 1]
+    scatterplot(x=xs, y=ys, hue=env.labels, ax=ax, s=10, linewidth=0)
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(VIS_OUPTUT_DIR, "env.png"))
+
+
+if __name__ == "__main__":
+    env = Env(N=100000, C=2, D=2)
+    plot_env(env)
