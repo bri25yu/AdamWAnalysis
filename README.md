08a660497926619e5d464ad79d698b116f2b965e
bri25yu <bri25yu@gmail.com>
2022-11-14 17:21:49 -0800

    updating readme

--- README.md
+++ README.md
@@ -1,42 +1,137 @@
 # AdamWAnalysis
 
 ### Example of an env
+We use a Voronoi partitioning of 2D space. We choose 2D space because it's easiest to visualize. We choose Voronoi partitions because it's very hard for a deep neural network to learn. 
+
 ![](vis_output/env.png)
 
 <div style="page-break-after: always;"></div>
 
 
+## Modeling
+All of the following equations use the following shared definitions.
+$$
+\begin{align*}
+& N = \text{batch size} \\
+& D = \text{dimension of environment} \\
+& X = \text{inputs to classify of shape (N, D)} \\
+& \mathcal{C} = \text{Voronoi centers} \\
+& C = \text{classes} \\
+& \theta = \text{model parameters} \\
+& \mathcal{L}(\hat{y}, y) = \text{loss function. Crossentropy in this case} \\
+& f_\theta = \text{classifier} \\
+& J_\theta = \text{objective function} = \mathcal{L}(f_\theta(X), y)\\
+\end{align*}
+$$
+
 ### Exact algorithm
+$$
+\begin{align*}
+& f_\theta = \argmin_C \left|\frac{X^T\mathcal{C}}{\|\mathcal{C}\|_2^2} - 1\right|
+\end{align*}
+$$
+
 ![](results/ExactAdamWExperiment/benchmark.gif)
 
 <div style="page-break-after: always;"></div>
 
 
 ### Learning which centers correspond to which class
+$$
+\begin{align*}
+& C_\theta = \text{Linear layer (} |\mathcal{C}||C| \text{ parameters)} \\
+& f_\theta = \text{softmax}\left(-\left|\frac{X^T\mathcal{C}}{\|\mathcal{C}\|_2^2} - 1\right|\right)C_\theta
+\end{align*}
+$$
+
 ![](results/CenterLabelsAdamWExperiment/benchmark.gif)
 
 <div style="page-break-after: always;"></div>
 
 
 ### ...And learning that values closer to 1 are better
+$$
+\begin{align*}
+& C_\theta = \text{Linear layer (} |\mathcal{C}||C| \text{ parameters)} \\
+& \text{offset}_\theta = \text{Offset (1 parameter)} \\
+& f_\theta = \text{softmax}\left(-\left|\frac{X^T\mathcal{C}}{\|\mathcal{C}\|_2^2} - \text{offset}_\theta\right|\right)C_\theta
+\end{align*}
+$$
+
 ![](results/LearnOffsetAdamWExperiment/benchmark.gif)
 
 <div style="page-break-after: always;"></div>
 
 
 ### ...And learning that larger scales leads to better softmax outputs
+$$
+\begin{align*}
+& C_\theta = \text{Linear layer (} |\mathcal{C}||C| \text{ parameters)} \\
+& \text{offset}_\theta = \text{Offset (1 parameter)} \\
+& \text{scale}_\theta = \text{Softmax conditioning scale (1 parameter)} \\
+& f_\theta = \text{softmax}\left(-\text{scale}_\theta \left|\frac{X^T\mathcal{C}}{\|\mathcal{C}\|_2^2} - \text{offset}_\theta\right|\right)C_\theta
+\end{align*}
+$$
+
 ![](results/OffsetScaleAdamWExperiment/benchmark.gif)
 
 <div style="page-break-after: always;"></div>
 
 
 ### ...And learning to use ReLU instead of absolute value
+$$
+\begin{align*}
+& C_\theta = \text{Linear layer (} |\mathcal{C}||C| \text{ parameters)} \\
+& \text{offset}_\theta = \text{Offset (1 parameter)} \\
+& \text{scale}_\theta = \text{Softmax conditioning scale (1 parameter)} \\
+& \text{pm}_\theta = \text{Plus-minus parameter for learning absolute value (2 parameters)} \\
+& f_\theta = \text{softmax}\left(-\text{scale}_\theta * \text{pm}_\theta * \text{ReLU}\left(\frac{X^T\mathcal{C}}{\|\mathcal{C}\|_2^2} - \text{offset}_\theta\right)\right)C_\theta
+\end{align*}
+$$
+
 ![](results/PlusMinusAdamWExperiment/benchmark.gif)
 
 <div style="page-break-after: always;"></div>
 
 
 ### ...And learning the centers from scratch
+$$
+\begin{align*}
+& C_\theta = \text{Linear layer (} |\mathcal{C}||C| \text{ parameters)} \\
+& \text{offset}_\theta = \text{Offset (1 parameter)} \\
+& \text{scale}_\theta = \text{Softmax conditioning scale (1 parameter)} \\
+& \text{pm}_\theta = \text{Plus-minus parameter for learning absolute value (2 parameters)} \\
+& \mathcal{C}_\theta = \text{Learned Voronoi centers (2048} D \text{ parameters)}\\
+& f_\theta = \text{softmax}\left(-\text{scale}_\theta * \text{pm}_\theta * \text{ReLU}\left(\frac{X^T\mathcal{C}_\theta}{\|\mathcal{C}_\theta\|_2^2} - \text{offset}_\theta\right)\right)C_\theta
+\end{align*}
+$$
+
 ![](results/CentersAdamWExperiment/benchmark.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+## Optimization
+
+### AdamW L2 weight decay (default)
+
+![](results/CustomAdamWExperiment/benchmark_vectors.gif)
+![](results/CustomAdamWExperiment/benchmark_scalars.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+### AdamW L1 weight decay
+
+![](results/AdamWL1Experiment/benchmark_vectors.gif)
+![](results/AdamWL1Experiment/benchmark_scalars.gif)
+
+<div style="page-break-after: always;"></div>
+
+
+### AdamW L1 and L2 proportional weight decay
+
+![](results/AdamWL1L2Experiment/benchmark_vectors.gif)
+![](results/AdamWL1L2Experiment/benchmark_scalars.gif)
 
 <div style="page-break-after: always;"></div>
--- awa/vis_mixins/logs_env_params.py
+++ awa/vis_mixins/logs_env_params.py
@@ -23,14 +23,13 @@ class LogsEnvParamsVisMixin:
 
         n_steps = len(self.vectors_to_plot_over_time)
         plot_steps = 100
+        writer = PillowWriter(fps=5)
 
         # List[Dict] -> Dict[str, List]
         transpose = lambda l: {k: [d[k] for d in l] for k in l[0]}
-        vector_logs = transpose(self.vectors_to_plot_over_time)
-        scalar_logs = transpose(self.scalars_to_plot_over_time)
 
-        def setup_fig_axs():
-            n_total_plots = len(vector_logs) + len(scalar_logs)
+        def setup_fig_axs(logs):
+            n_total_plots = len(logs)
             rows, cols = (n_total_plots // 2) + (n_total_plots % 2), 2
             fig, axs = subplots(rows, cols, figsize=(10 * cols, 8 * rows), dpi=200)
 
@@ -39,34 +38,23 @@ class LogsEnvParamsVisMixin:
                 fig.delaxes(axs[-1])
                 axs = axs[:-1]
 
-            vector_axs = axs[:len(vector_logs)]
-            scalar_axs = axs[len(vector_logs):]
-
             fig.suptitle(f"Final benchmark for {self.name}")
 
-            return fig, vector_axs, scalar_axs
+            return fig, axs
 
-        fig, vector_axs, scalar_axs = setup_fig_axs()
-
-        # Set the scalar axs labels
-        for ax, value_name in zip(scalar_axs, scalar_logs):
-            ax.set_xlabel("Train step")
-            ax.set_title(value_name)
-
-        def plot_scalars(step: int):
+        def plot_scalars(step: int, scalar_axs, scalar_logs):
             steps = list(range(step+1))
             artists = []
-            for ax, values in zip(scalar_axs, scalar_logs.values()):
+            for ax, (value_name, values) in zip(scalar_axs, scalar_logs.items()):
+                ax.set_xlabel("Train step")
+                ax.set_title(value_name)
                 artists.extend(
                     ax.plot(steps, values[:step+1], color="C0")
                 )
 
             return artists
 
-        def plot_logits(step: int):
-            # Scatterplot of 2D env
-            logits_ax = vector_axs[0]
-
+        def plot_logits(step: int, logits_ax, vector_logs):
             logits: ndarray = vector_logs["Eval logits"][step]
 
             preds = logits.argmax(axis=1)
@@ -94,14 +82,13 @@ class LogsEnvParamsVisMixin:
 
             return artists
 
-        def plot_vectors(step: int):
-            axs = vector_axs[1:]
+        def plot_vectors(step: int, vector_axs, vector_logs):
             logs = {
                 k: v[step] for k, v in vector_logs.items() if k != "Eval logits"
             }
 
             artists = []
-            for ax, (value_name, values) in zip(axs, logs.items()):
+            for ax, (value_name, values) in zip(vector_axs, logs.items()):
                 xs = values[:, 0]
                 ys = values[:, 1]
                 artists.append(ax.scatter(xs, ys, color="C0"))
@@ -116,24 +103,37 @@ class LogsEnvParamsVisMixin:
 
             return artists
 
-        artists = []
-        for step in trange(0, n_steps, plot_steps, leave=False, desc="Visualizing"):
-            artists.append([
-                *plot_scalars(step),
-                *plot_logits(step),
-                *plot_vectors(step),
-            ])
+        def save_fig(fig, artists, output_path, desc):
+            with tqdm(total=n_steps // plot_steps, desc=f"Drawing and saving {desc}", leave=False) as pbar:
+                update_pbar = lambda current_step, total_steps: pbar.update(1)
 
-        with tqdm(total=n_steps // plot_steps, desc="Drawing and saving", leave=False) as pbar:
-            update_pbar = lambda current_step, total_steps: pbar.update(1)
+                animation = ArtistAnimation(fig, artists, interval=1, blit=True)
+                animation.save(output_path, writer=writer, progress_callback=update_pbar)
 
-            animation = ArtistAnimation(fig, artists, interval=1, blit=True)
+            close(fig)
 
-            writer = PillowWriter(fps=5)
-            output_path = os.path.join(RESULTS_DIR, self.name, "benchmark.gif")
-            animation.save(output_path, writer=writer, progress_callback=update_pbar)
+        # Visualize scalar data
+        scalar_logs = transpose(self.scalars_to_plot_over_time)
+        scalar_fig, scalar_axs = setup_fig_axs(scalar_logs)
+        scalars_output_path = os.path.join(RESULTS_DIR, self.name, "benchmark_scalars.gif")
+        scalar_artists = [
+            plot_scalars(step, scalar_axs, scalar_logs)
+            for step in trange(0, n_steps, plot_steps, leave=False, desc="Visualizing scalars")
+        ]
+        save_fig(scalar_fig, scalar_artists, scalars_output_path, "scalars")
 
-        close()
+        # Visualize vector data
+        vector_logs = transpose(self.vectors_to_plot_over_time)
+        vector_fig, vector_axs = setup_fig_axs(vector_logs)
+        vectors_output_path = os.path.join(RESULTS_DIR, self.name, "benchmark_vectors.gif")
+        vector_artists = [
+            [
+                *plot_logits(step, vector_axs[0], vector_logs),
+                *plot_vectors(step, vector_axs[1:], vector_logs),
+            ]
+            for step in trange(0, n_steps, plot_steps, leave=False, desc="Visualizing vectors")
+        ]
+        save_fig(vector_fig, vector_artists, vectors_output_path, "vectors")
 
     def setup_visualization_logging(self) -> None:
         self.vectors_to_plot_over_time = []  # 2D vectors
